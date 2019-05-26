0092a09c39c321a7768c4b334c6abc3b2e2f8600
fcschmidt <fcschimidt@gmail.com>
2019-05-26 17:21:44 -0300

    Created Model Prescriptions

---
+++ health/app/blueprints/api/models.py
@@ -0,0 +1,20 @@
+from health.app.ext.db import db
+
+
+class Prescriptions(db.Model):
+
+    __tablename__ = "prescriptions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    clinic_id = db.Column(db.Integer, nullable=True)
+    physician_id = db.Column(db.Integer, nullable=True)
+    patient_id = db.Column(db.Integer, nullable=True)
+    text = db.Column(db.String(100), nullable=True)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'data(id={self.id}, clinic_id={self.clinic_id}, ' \
+            f'physician_id={self.physician_id}, patient_id={self.patient_id}, text={self.text})'
