cf4f75c09541c9c34efbeeae2208037d6a1c4983
fcschmidt <fcschimidt@gmail.com>
2019-05-27 16:05:23 -0300

    Reformated Errors and added status code

--- health/app/blueprints/api/errors.py
+++ health/app/blueprints/api/errors.py
@@ -1,67 +1,79 @@
 """
 Custom Errors: status code and Message
 """
+from flask import jsonify
 
 
 class ErrorResponse(object):
 
-    def __init__(self, code: str, message: str):
+    def __init__(self, code_error: str, message: str, status_code: int):
+        self.code_error = code_error
         self.message = message
-        self.code = code
+        self.status_code = status_code
 
     def get_error_response(self):
-        error = {
-            "error": {
-                "message": self.message,
-                "code": self.code
+        error_response = jsonify(
+            {
+                "error": {
+                    "code": self.code_error,
+                    "message": self.message
+
+                }
             }
-        }
-        return error
+        )
+        error_response.status_code = self.status_code
+        return error_response
 
 
 def malformed_request():
     error_response = ErrorResponse(
+        "01",
         "malformed request",
-        "01"
+        400
     )
     return error_response.get_error_response()
 
 
 def physician_not_found():
     error_response = ErrorResponse(
+        "02",
         "physician not found",
-        "02"
+        404
     )
     return error_response.get_error_response()
 
 
 def patient_not_found():
     error_response = ErrorResponse(
+        "03",
         "patient not found",
-        "03"
+        404
     )
     return error_response.get_error_response()
 
 
 def metrics_service_not_available():
     error_response = ErrorResponse(
+        "04",
         "metrics service not available",
-        "04"
+        400
     )
     return error_response.get_error_response()
 
 
 def physicians_service_not_available():
     error_response = ErrorResponse(
+        "05",
         "physicians service not available",
-        "05"
+        400
     )
     return error_response.get_error_response()
 
 
 def patients_service_not_available():
     error_response = ErrorResponse(
+        "06",
         "patients service not available",
-        "06"
+        400
     )
     return error_response.get_error_response()
