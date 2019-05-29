cd1e4ebc8a0dc06d5da1ddcaf40dacc940447353
fcschmidt <fcschimidt@gmail.com>
2019-05-29 15:33:22 -0300

    Added timeout request and created post request metrics

--- health/services/consumer_services.py
+++ health/services/consumer_services.py
@@ -1,25 +1,31 @@
 import requests
 import os
-from health.authorization_keys import (
-    PATIENTS_AUTH,
-    PHYSICIANS_AUTH,
-    CLINICS_AUTH,
-    METRICS_AUTH
+
+from health.authorization_keys_test import (
+    PATIENTS_AUTH_TEST,
+    PHYSICIANS_AUTH_TEST,
+    CLINICS_AUTH_TEST,
+    METRICS_AUTH_TEST
     )
 
 
 class RequestService:
 
-    def __init__(self, host, authorization):
+    def __init__(self, host, authorization, timeout):
         self.service_host = host
         self.authorization = authorization
+        self.timeout = timeout
         self.request = requests
 
     def get_service(self):
         header = {
             'Authorization': self.authorization
         }
-        response = self.request.get(self.service_host, headers=header)
+
+        try:
+            response = self.request.get(self.service_host, headers=header, timeout=self.timeout)
+        except requests.ConnectTimeout:
+            return '408'
 
         if response.status_code == 404:
             return '404'
@@ -34,8 +40,14 @@ class RequestService:
             'Authorization': self.authorization,
             'Content-Type': 'application/json'
         }
-        response = self.request.post(self.service_host,  headers=header, json=data)
-        return response
+
+        try:
+            response = self.request.post(
+                self.service_host,  headers=header, json=data, timeout=self.timeout)
+        except requests.ConnectTimeout:
+            return '408'
+
+        return response.json()
 
 
 class ConsumerService:
@@ -46,11 +58,12 @@ class ConsumerService:
 
         r = RequestService(
             service_host,
-            os.getenv('CLINICS_AUTH') or CLINICS_AUTH
+            os.getenv('CLINICS_AUTH') or CLINICS_AUTH_TEST,
+            4
         )
         response = r.get_service()
 
-        return response.json()
+        return response
 
     @staticmethod
     def get_physicians(_id: int):
@@ -58,7 +71,8 @@ class ConsumerService:
 
         r = RequestService(
             service_host,
-            os.getenv('PHYSICIANS_AUTH') or PHYSICIANS_AUTH
+            os.getenv('PHYSICIANS_AUTH') or PHYSICIANS_AUTH_TEST,
+            5
         )
 
         response = r.get_service()
@@ -66,16 +80,30 @@ class ConsumerService:
         return response
 
     @staticmethod
-    def get_patient(_id: int):
+    def get_patients(_id: int):
         service_host = f"https://limitless-shore-81569.herokuapp.com/v3/patients/{_id}"
 
         r = RequestService(
             service_host,
-            os.getenv('PATIENTS_AUTH') or PATIENTS_AUTH
+            os.getenv('PATIENTS_AUTH') or PATIENTS_AUTH_TEST,
+            3
         )
 
         response = r.get_service()
 
         return response
 
+    @staticmethod
+    def post_metrics(data: dict):
+        service_host = 'https://mysterious-island-73235.herokuapp.com/api/metrics'
+
+        r = RequestService(
+            service_host,
+            os.getenv('METRICS_AUTH') or METRICS_AUTH_TEST,
+            6
+        )
+
+        response = r.post_service(data)
+
+        return response
 
