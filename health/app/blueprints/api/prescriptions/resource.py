9e22ee9d9b33675bb5e18a12c522e4d80d6620fe
fcschmidt <fcschimidt@gmail.com>
2019-05-30 18:24:00 -0300

    Updated and final version

--- health/app/blueprints/api/prescriptions/resource.py
+++ health/app/blueprints/api/prescriptions/resource.py
@@ -1,23 +1,19 @@
 from flask import Blueprint
 from flask_restful import Api, reqparse, Resource, fields
 
-from health.app.blueprints.api.errors import (
-    malformed_request,
-    patient_not_found,
-    patients_service_not_available,
-    physician_not_found,
-    physicians_service_not_available,
-    metrics_service_not_available,
-    invalid_syntax,
-    clinic_not_found,
-    timeout_request
-    )
-
-
 from health.services.consumer_services import ConsumerService
 
 from health.app.blueprints.api.models import Prescription as PrescriptionModel
-from health.app.blueprints.api.utils import serializer
+
+from health.app.blueprints.api.utils import (
+    patients_response_parser,
+    physicians_response_parser,
+    clinics_response_parser,
+    metrics_response_parser,
+    prescription_parser,
+    serializer
+    )
+
 from health.app.blueprints.api.responses import resp_content_successfully
 
 bp = Blueprint('prescriptions_api', __name__, url_prefix='/api/v2')
@@ -50,56 +46,29 @@ class PrescriptionsAPI(Resource):
         physician = self.args['physician']
         text = self.args['text']
 
-        if not clinic:
-            return malformed_request()
+        response_parser = prescription_parser([clinic, patient, physician, text])
+        if response_parser:
+            return response_parser
 
-        if not patient:
-            return malformed_request()
+        patients_response = self.service.get_patients(patient['id'])
+        physicians_response = self.service.get_physicians(physician['id'])
+        clinics_response = self.service.get_clinic(clinic['id'])
 
-        if not physician:
-            return malformed_request()
+        response_patients_parser = patients_response_parser(patients_response)
+        if response_patients_parser:
+            return response_patients_parser
 
-        if not text:
-            return malformed_request()
+        response_physicians_parser = physicians_response_parser(physicians_response)
+        if response_physicians_parser:
+            return response_physicians_parser
 
-        patient_response = self.service.get_patients(patient['id'])
-        physician_response = self.service.get_physicians(physician['id'])
-        clinic_response = self.service.get_clinic(clinic['id'])
+        response_clinics_parser = clinics_response_parser(clinics_response)
+        if response_clinics_parser:
+            return response_clinics_parser
 
-        if patient_response == '404':
-            return patient_not_found()
-
-        if patient_response == '400':
-            return invalid_syntax()
-
-        if patient_response == '408':
-            return timeout_request('Patients')
-
-        if patient_response == '503':
-            return patients_service_not_available()
-
-        if physician_response == '404':
-            return physician_not_found()
-
-        if physician_response == '400':
-            return invalid_syntax()
-
-        if physician_response == '408':
-            return timeout_request('Physicians')
-
-        if physician_response == '503':
-            return physicians_service_not_available()
-
-        if clinic_response == '404':
-            return clinic_not_found()
-
-        if clinic_response == '400':
-            return invalid_syntax()
-
-        clinic_name = clinic_response['data']['name']
-        clinic_id = clinic_response['data']['id']
-
-        if clinic_response == '408':
+        clinic_name = clinics_response['data']['name']
+        clinic_id = clinics_response['data']['id']
+        if clinics_response == '408':
             clinic_name = ''
             clinic_id = clinic['id']
 
@@ -115,28 +84,19 @@ class PrescriptionsAPI(Resource):
         data_compose = {
             'clinic_id': clinic_id,
             'clinic_name': clinic_name,
-            'physician_id': physician_response['data']['id'],
-            'physician_name': physician_response['data']['fullName'],
-            'physician_crm': physician_response['data']['crm'],
-            'patient_id':  patient_response['data']['id'],
-            'patient_name': patient_response['data']['fullName'],
-            'patient_email': patient_response['data']['email'],
-            'patient_phone': patient_response['data']['phone']
+            'physician_id': physicians_response['data']['id'],
+            'physician_name': physicians_response['data']['fullName'],
+            'physician_crm': physicians_response['data']['crm'],
+            'patient_id':  patients_response['data']['id'],
+            'patient_name': patients_response['data']['fullName'],
+            'patient_email': patients_response['data']['email'],
+            'patient_phone': patients_response['data']['phone']
         }
 
         metrics_response = self.service.post_metrics(data_compose)
-
-        if metrics_response == '400':
-            query_prescription.delete()
-            return malformed_request()
-
-        if metrics_response == '408':
-            query_prescription.delete()
-            return timeout_request('Metrics')
-
-        if metrics_response == '503':
-            query_prescription.delete()
-            return metrics_service_not_available()
+        response_metrics_parser = metrics_response_parser(metrics_response, query_prescription)
+        if response_metrics_parser:
+            return response_metrics_parser
 
         serialized = serializer(query_prescription, False)
         return resp_content_successfully(serialized)
