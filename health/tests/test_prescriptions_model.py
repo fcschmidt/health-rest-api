aca5934bdee205cfad13341797b2ad6d02e75565
fcschmidt <fcschimidt@gmail.com>
2019-08-03 12:55:59 -0300

    Refactored and created new tests

--- health/tests/test_prescriptions_model.py
+++ health/tests/test_prescriptions_model.py
@@ -5,7 +5,8 @@ from health.app.blueprints.api.models import Prescription
 @pytest.mark.usefixtures('session')
 class TestPrescriptionsModel:
 
-    def test_create_prescription(self):
+    @staticmethod
+    def create_prescription():
         prescription = Prescription(
             clinic_id=1,
             physician_id=1,
@@ -13,5 +14,21 @@ class TestPrescriptionsModel:
             text="Dipirona 1x ao dia"
         )
         prescription.save()
-        prescription_id = prescription.id
-        assert prescription_id == 1
+        return prescription
+
+    def test_create_prescription(self):
+        prescription = self.create_prescription()
+        assert prescription.id == 1
+
+    def test_get_prescription(self):
+        prescription = self.create_prescription()
+        assert prescription.id == 1
+
+        get_prescription = prescription.get_prescription(1)
+        assert prescription == get_prescription
+
+    def test_delete_prescription(self):
+        prescription = self.create_prescription()
+        prescription.delete()
+        get_prescription = prescription.get_prescription(1)
+        assert get_prescription is None
